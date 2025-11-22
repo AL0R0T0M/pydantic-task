2d71b19c1cef7d699f0fc1ca99dbccbff0e644ca
AL0R0T0M <147801355+AL0R0T0M@users.noreply.github.com>
2025-11-22 23:47:27 +0300

    Add files via upload

---
+++ const.py
@@ -0,0 +1,65 @@
+data = {
+    'id': 0,
+    'title': 'Тестовая Сделка',
+    'comment': 'Тестовая сделка',
+    'created_at': '2025-11-16T12:00:00',
+    'persons_in_charge': [{
+        'id': 0,
+        'name': 'Igor',
+        'age': 18,
+        'is_supervisor': True,
+        'email': 'test@mail.ru',
+        'phone_number': '+7 (996) 918-57-51'
+    }],
+    'deal_type': {
+        'buy': 'test_buy_value',
+        'sell': 'test_sell_value'
+    }
+}
+
+data2 = {
+    'id': 0,
+    'name': 'Sergey',
+    'age': 23,
+    'is_supervisor': False,
+    'email': 'test2222@mail.ru',
+    'phone_number': '+7 (800) 555-35-35'
+}
+
+data3 = {
+    'id': 0,
+    'title': 'Тестовая Сделка (обновлено)',
+    'comment': 'Изменённая тестовая сделка',
+    'created_at': '2025-12-16T12:30:00',
+    'persons_in_charge': [{
+        'id': 1,
+        'name': 'Maxim',
+        'age': 44,
+        'is_supervisor': True,
+        'email': 'MAXIM@mail.ru',
+        'phone_number': '+7 (900) 963-58-61'
+    }],
+    'deal_type': {
+        'buy': 'test_buy_value',
+        'sell': 'test_sell_value'
+    }
+}
+
+data4 = {
+    'id': 0,
+    'title': 'Тестовая Сделка',
+    'comment': 'Тестовая сделка',
+    'created_at': '2025-11-16T12:00:00',
+    'persons_in_charge': [{
+        'id': 0,
+        'name': 'Igor',
+        'age': 18,
+        'is_supervisor': False,
+        'email': 'test@mail.ru',
+        'phone_number': '89999635754'
+    }],
+    'deal_type': {
+        'buy': 'test_buy_value',
+        'sell': 'test_sell_value'
+    }
+}
